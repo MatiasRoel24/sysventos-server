24a4cce6f89d9a62e750fb3d5aac752991772467
Matias Roel <matiasroel241201@gmail.com>
2025-11-24 17:55:00 -0300

    CRUD - Orders

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,7 +1,14 @@
-import { IsArray, IsUUID } from "class-validator";
+import { Type } from 'class-transformer';
+import { IsArray, ValidateNested, ArrayMinSize } from 'class-validator';
+import { CreateOrderItemDto } from './create-order-item.dto';
 
+/**
+ * DTO para crear un nuevo pedido
+ */
 export class CreateOrderDto {
-
     @IsArray()
-    readonly products: any;
+    @ArrayMinSize(1, { message: 'El pedido debe contener al menos un producto' })
+    @ValidateNested({ each: true })
+    @Type(() => CreateOrderItemDto)
+    items: CreateOrderItemDto[];
 }
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,12 +3,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderStatus } from './entities/order-status.entity';
 import { Order } from './entities/order.entity';
 import { OrderItem } from './entities/order-item.entity';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
+import { OrderStatusService } from './services/order-status.service';
+import { OrdersService } from './services/orders.service';
+import { KitchenOrdersService } from './services/kitchen-orders.service';
+import { OrdersController } from './controllers/orders.controller';
+import { KitchenOrdersController } from './controllers/kitchen-orders.controller';
+import { EventsModule } from '../events/events.module';
+import { InventoriesModule } from '../inventories/inventories.module';
+import { ProductsModule } from '../products/products.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order, OrderStatus, OrderItem])],
-  controllers: [OrdersController],
-  providers: [OrdersService],
+  imports: [
+    TypeOrmModule.forFeature([Order, OrderStatus, OrderItem]),
+    EventsModule,
+    InventoriesModule,
+    ProductsModule,
+    AuthModule,
+  ],
+  controllers: [OrdersController, KitchenOrdersController],
+  providers: [OrderStatusService, OrdersService, KitchenOrdersService],
+  exports: [OrdersService, KitchenOrdersService],
 })
-export class OrdersModule {}
+export class OrdersModule { }
